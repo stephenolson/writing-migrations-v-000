6e835ed650e7fa0784b0e237d9d59523e898c479
Stephen Olson <stephenolson@gmail.com>
2019-06-02 04:34:02 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
     change_column :students, :birthdate, :integer
   end
